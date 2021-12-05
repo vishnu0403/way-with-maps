0418797cb5d522562ff17d16dc7465d7ede94b8b
vishnu0403 <93178147+vishnu0403@users.noreply.github.com>
2021-12-05 16:45:04 +0530

    Add files via upload

---
+++ triple function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+nums = (1, 2, 3, 4, 5, 6, 7) 
+print("Original list: ", nums)
+result = map(lambda x: x + x + x, nums) 
+print("\nTriple of said list numbers:")
+
+
+# In[7]:
+
+
+print(list(result))
+
+
+# In[ ]:
+
+
+
+
